5876351d695ed29786ef2c9fb17982d92c2757ff
pawanlalganesh <pawanlalganesh@gmail.com>
2022-05-04 19:01:56 +0545

    add: questions hints

--- questions/question_2022_may_4.js
+++ questions/question_2022_may_4.js
@@ -3,7 +3,12 @@
 // Extract the first half a
 // Return the result
 
+// Input : "abcdef" output: "abc"
+
 // Question 2
 // Write a function that takes an object (a) and a string (b) as argument
 // Return true if a has a property with key b
 // Return false otherwise
+
+// Input : {a: 1, b: 2} "a" output: true
+// Input : {a: 1, b: 2} "c" output: false

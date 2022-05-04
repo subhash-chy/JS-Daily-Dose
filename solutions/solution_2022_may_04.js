6fd72a05c1caaafd67442255f835e2cdc4364c18
subash <imsuubash@gmail.com>
2022-05-04 21:05:03 +0545

    2022 May 04

--- solutions/solution_2022_may_04.js
+++ solutions/solution_2022_may_04.js
@@ -1,4 +1,9 @@
 // Question 1
+// Write a function that takes a string (a) as argument
+// Extract the first half a
+// Return the result
+
+// Input : "abcdef" output: "abc"
 const extractHalf = (a) => {
   if (typeof a == "string") {
     const x = a.substring(0, a.length / 2);
@@ -11,9 +16,13 @@ const r = extractHalf("abcdef");
 console.log(r);
 
 // Question 2
+// Write a function that takes an object (a) and a string (b) as argument
+// Return true if a has a property with key b
+// Return false otherwise
+
+// Input : {a: 1, b: 2} "a" output: true
+// Input : {a: 1, b: 2} "c" output: false
 const boolReturner = (a, b) => {
-  // a= {a: 1, b:2}
-  // a= {}, b= ""
   if (b in a) {
     return true;
   }

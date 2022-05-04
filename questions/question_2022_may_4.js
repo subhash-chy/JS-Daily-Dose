ed2d272277080b3e53db922e4f0738acda5517fe
pawanlalganesh <pawanlalganesh@gmail.com>
2022-05-04 18:49:48 +0545

    add: files

---
+++ questions/question_2022_may_4.js
@@ -0,0 +1,9 @@
+// Question 1
+// Write a function that takes a string (a) as argument
+// Extract the first half a
+// Return the result
+
+// Question 2
+// Write a function that takes an object (a) and a string (b) as argument
+// Return true if a has a property with key b
+// Return false otherwise

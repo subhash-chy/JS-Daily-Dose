afbe146eeb1d32a280e836822b3a0c118e35ef89
subash <imsuubash@gmail.com>
2022-05-17 22:00:36 +0545

    2022-May-17

---
+++ questions/question_2022_May_17.js
@@ -0,0 +1,15 @@
+// Question 1
+// Write a function that takes an array (a) and a number (n) as arguments
+// It should return the last n elements of a
+
+// Input: myFunction([1, 2, 3, 4, 5, 6 ], 3)
+// Output: [4, 5, 6]
+
+// Question 2
+// Write a function that takes two numbers (min and max) as arguments
+// Return an array of numbers in the range min to max
+// Input: myFunction(2, 4)
+// Output: [2, 3, 4]
+
+// Input: myFunction(-5, 3)
+// Output: [-5, -4, -3, -2, -1, 0, 1, 2, 3]

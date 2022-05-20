12c3f1f41576671bc18e23c0382a65c817cae32b
subash <imsuubash@gmail.com>
2022-05-20 13:51:12 +0545

    2022-May-09

---
+++ questions/question_2022_May_09.js
@@ -0,0 +1,13 @@
+// Question 1
+// A function that takes two arguments `a` and `b` as a number. Print the number that is nearest to 100. If both the numbers are nearest, then print message ``They both are nearest``
+// Input: myFunction(20, 78)      // Output: 78
+// Input: myFunction(101, 98)     // Output: 101
+// Input: myFunction(98, 120)     // Output: 98
+// Input: myFunction(99, 101)     // Output: They both are nearest
+
+// Question 2
+// Write a function to check if a given string contains 2 to 4 occurences of a specified characters.
+// Input: myFunction("bo", "o")       // Output: false
+// Input: myFunction("bbo", "b")      // Output: true
+// Input: myFunction("boooo", "o")    // Output: true
+// Input: myFunction("booooo", "o")   // Output: false

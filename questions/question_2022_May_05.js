db82bd2526d830e5af506f3b3b707f8f7fe5ca4d
subash <imsuubash@gmail.com>
2022-05-20 13:22:10 +0545

    2022-May-05 to 06

---
+++ questions/question_2022_May_05.js
@@ -0,0 +1,23 @@
+// Question 1
+// Flatten the array
+/*
+let arr = [
+  [1, 2],
+  [3, 4],
+  [5, 6, [7, 8], 9, 10],
+  [11, 12],
+];
+*/
+
+// Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+// Question 2
+/*
+Write a function that takes two arguments 'a' and 'b' as a number. Return `true` if one of the number is 100 or if the sum of `a` and `b` is 100.
+Return `false` if above condition is not met.
+*/
+// Input: myFunction(100, 2)  // Output: true
+// Input: myFunction(0, 100)  // Output: true
+// Input: myFunction(40, 10)  // Output: false
+// Input: myFunction(40, 60)  // Output: true
+// Input: myFunction(150, 10) // Output: false
---
+++ questions/question_2022_May_06.js
@@ -0,0 +1,13 @@
+// Question 1
+/*
+Write a function to get a extension of a file.
+*/
+// Input: myFunction("index.html")  // Output: html
+// Input: myFunction("virus.exe")   // Output: exe
+// Input: myFunction("con.py.rs")   // Output: rs
+
+// Question 2
+// Write a function that takes an argument as string. Replace every character in a given string with the character following it in the alphabet.
+// Input: myFunction("abcd")  // Output: bcde
+// Input: myFunction("wxyz")  // Output: xyza
+// Input: myFunction("defg")  // Output: efgh

5dc30fa7b7236d4550310f0207504ed50a68eece
pawanlalganesh <pawanlalganesh@gmail.com>
2022-05-21 21:36:45 +0545

    2020 may 5 question solved

--- solutions/solution_2022_may_5.js
+++ solutions/solution_2022_may_5.js
@@ -11,6 +11,22 @@ let arr = [
 
 // Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
+let arr = [
+  [1, 2],
+  [3, 4],
+  [5, 6, [7, 8], 9, 10],
+  [11, 12],
+];
+
+const result = arr.reduce(
+  (accumulator, currentValue) => accumulator.concat(...currentValue),
+  []
+);
+
+console.log(result);
+
+// Similar problem => https://flexiple.com/flatten-array-javascript/
+
 // Question 2
 /*
 Write a function that takes two arguments 'a' and 'b' as a number. Return `true` if one of the number is 100 or if the sum of `a` and `b` is 100.
@@ -21,3 +37,13 @@ Return `false` if above condition is not met.
 // Input: myFunction(40, 10)  // Output: false
 // Input: myFunction(40, 60)  // Output: true
 // Input: myFunction(150, 10) // Output: false
+
+const myFunction = (a, b) => {
+  return a | (b === 100) && a + b === 100;
+};
+
+console.log(myFunction(100, 0));
+console.log(myFunction(0, 100));
+console.log(myFunction(40, 10));
+console.log(myFunction(40, 60));
+console.log(myFunction(150, 10));

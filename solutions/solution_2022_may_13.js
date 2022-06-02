0d918c75d4e43188e361abb73eb114787d1ea6b2
subash <imsuubash@gmail.com>
2022-06-02 22:25:19 +0545

    sol 13 may 2022

--- solutions/solution_2022_may_13.js
+++ solutions/solution_2022_may_13.js
@@ -12,6 +12,14 @@ myFunction([-10, 22, 333, 42], [-11, 5, 22, 41, 42])
 Expected
 [ -11, -10, 5, 22, 41,  42, 333]
 */
+const arrayMerger = (arr1, arr2) => {
+  let arr = [...arr1, ...arr2];
+  let uniqueArr = [...new Set(arr)];
+  return uniqueArr.sort((a, b) => a - b);
+};
+
+const am = arrayMerger([-10, 22, 333, 42], [-11, 5, 22, 41, 42]);
+console.log(am);
 
 // Question 2
 /*
@@ -28,3 +36,12 @@ myFunction([4,-3,2,1,0])
 Expected
 1
 */
+const number_of_negative_numbers = (arr) => {
+  const negative_numbers = arr.filter((num) => {
+    return num < 0;
+  });
+
+  console.log("Number of negative numbers => ", negative_numbers.length);
+};
+
+number_of_negative_numbers([1, -2, 2, -4]);
